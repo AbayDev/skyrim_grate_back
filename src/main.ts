ddc453e3dcbd8e1c77d4afe880b1bd3624c1cd17
Abay Kinayat <abay.kinayat@simourg.com>
2025-08-30 13:51:04 +0500

    feat: добавил куку refresh token для регистрации

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
+  app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/auth/presentation/controller/auth.controller.ts
+++ src/modules/auth/presentation/controller/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { RegisterCommand } from '../../application/command/register/register.command';
 import { RegisterDto } from '../../dto/register.dto';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('/auth')
 export class AuthController {
@@ -12,19 +12,32 @@ export class AuthController {
   authenticate() {}
 
   @Post('/registration')
-  registration(@Req() req: Request, @Body() data: RegisterDto) {
+  async registration(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() data: RegisterDto,
+  ) {
     const forwardedIps = req.headers['x-forwarded-for'];
 
     // Берём первый IP-адрес из списка (если он есть), иначе используем request.ip
     const ip = forwardedIps ? forwardedIps.toString().split(',')[0] : req.ip;
     const userAgent = req.headers['user-agent'];
 
-    return this.commandBus.execute(
+    const result = await this.commandBus.execute(
       new RegisterCommand(data.nickname, data.password, {
         ip,
         userAgent,
       }),
     );
+
+    res.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    return res.status(200).json(result);
   }
 
   @Post('/logout')
